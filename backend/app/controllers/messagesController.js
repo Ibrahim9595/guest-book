aade2a196491bb3f5c731bc252e71b76f2af8f1c
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-11 23:13:40 +0200

    write create, delete and update logic

---
+++ backend/app/controllers/messagesController.js
@@ -0,0 +1,66 @@
+import { BaseController } from './baseController';
+import { MessageModel } from '../models/messageModel';
+import { ObjectId } from 'mongodb';
+
+class MessagesController extends BaseController {
+
+    async read(req, res, next) {
+
+    }
+
+    async create(req, res, next) {
+        try {
+            const { text } = req.body;
+            const user = req.user;
+
+            const message = await MessageModel.create({
+                text,
+                user_id: ObjectId(user._id)
+            });
+
+            this.success(res, 201, message);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async update(req, res, next) {
+        try {
+            const { text } = req.body;
+            const user = req.user;
+            const { id } = req.params;
+
+            const message = await MessageModel.update(id, { text }, { user_id: ObjectId(user._id) })
+
+            if (message) {
+                this.success(res, 200, message);
+            } else {
+                this.failed(res, 400, 'BAD_MESSAGE_REQUEST');
+            }
+
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const user = req.user;
+            const { id } = req.params;
+
+            const message = await MessageModel.delete(id, { user_id: ObjectId(user._id) })
+
+            if (message) {
+                this.success(res, 200, message);
+            } else {
+                this.failed(res, 400, 'BAD_MESSAGE_REQUEST');
+            }
+
+        } catch (error) {
+            next(error);
+        }
+    }
+
+}
+
+export const messagesController = new MessagesController();
