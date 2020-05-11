8ea6d7bdfb8bc9de39a4b184e3ee69c95f7e7f5e
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-11 14:51:22 +0200

    write cors middleware

---
+++ backend/app/middlewares/cors.js
@@ -0,0 +1,6 @@
+export const CORS = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+}
