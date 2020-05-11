94e737662343d79ae4dd3205124fe85c3959f428
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-11 21:56:43 +0200

    add faild method for failed requests

--- backend/app/controllers/baseController.js
+++ backend/app/controllers/baseController.js
@@ -2,4 +2,8 @@ export class BaseController {
     success(response, status, data) {
         response.status(status).json({ status: 'SUCCESS', data });
     }
+
+    failed(response, status, error) {
+        response.status(status).json({ status: 'ERROR', error });
+    }
 }
