137248183f3e5e1ec20da03c0a78fbf0942f349e
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-12 01:32:04 +0200

    write code for routing and middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,36 @@
-// import config from './app/config';
-// import { CORS, ErrorHandler, AUTH } from './app/middlewares';
+import config from './app/config';
+import { CORS, ErrorHandler, AUTH, validator } from './app/middlewares';
+import { messageRules, loginRules, registerRules } from './app/validations';
+import { authController } from './app/controllers/authController';
+import { messagesController } from './app/controllers/messagesController';
+import { repliesController } from './app/controllers/repliesController';
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// const port = config[config.env].port;
-// const app = express();
+const port = config[config.env].port;
+const app = express();
 
-// app.use(bodyParser.json());
-// app.use(CORS);
-// app.use(AUTH);
+app.use(bodyParser.json());
+app.use(CORS);
 
+// Auth Routes
+app.post('/register', validator(registerRules), authController.register.bind(authController));
+app.post('/login', validator(loginRules), authController.login.bind(authController));
+app.post('/logout', AUTH, authController.logout.bind(authController));
 
-// app.post('/', (req, res) => {
-//     res.json({ data: 'Test' })
-// })
+// Messages Routes
+app.get('/message', AUTH, messagesController.read.bind(messagesController))
+app.post('/message', AUTH, validator(messageRules), messagesController.create.bind(messagesController));
+app.put('/message/:id', AUTH, validator(messageRules), messagesController.update.bind(messagesController));
+app.delete('/message/:id', AUTH, messagesController.delete.bind(messagesController));
 
-// app.use(ErrorHandler);
+// Replies Routes
+app.get('/message/:message_id/replies', AUTH, repliesController.read.bind(repliesController))
+app.post('/message/:message_id/replies', AUTH, validator(messageRules), repliesController.create.bind(repliesController));
+app.put('/replies/:id', AUTH, validator(messageRules), repliesController.update.bind(repliesController));
+app.delete('/replies/:id', AUTH, repliesController.delete.bind(repliesController));
 
-// app.listen(port, () => console.log(`app is running on http://localhost:${port}`))
+app.use(ErrorHandler);
 
-
-// validate({
-//     email: '1234@test.com',
-//     password: '12345678',
-//     name: 'afasf'
-// }, {
-//     email: ['required', 'email'],
-//     name: ['required', 'string'],
-//     password: ['required', 'string', 'minLength:8']
-// })
+app.listen(port, () => console.log(`app is running on http://localhost:${port}`))
