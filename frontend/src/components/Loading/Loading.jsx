767e40781086b2a0e0210ab21703db189f4c372b
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-12 22:42:58 +0200

    create a loading indicator

---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loading.css';
+
+export const Loading = _ => (
+    <div className="app-loading-container">
+        <div className="app-loading-indicator"></div>
+    </div>
+)
