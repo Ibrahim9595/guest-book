afd7450e4b856d26be3b324e0b9d790d1d2a1a05
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 23:19:19 +0200

    create navbar component

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = ({ user, onLogout, onWriteMessage }) => (
+	<div className="nav-bar">
+		<div className="nav-bar-left">
+			<button className="action-buttons" onClick={onWriteMessage} title="Write Message">✎</button>
+		</div>
+		<div className="logo-container">
+			<span className="heavy">Guest Book</span>
+		</div>
+		<div className="nav-bar-right">
+			<button className="action-buttons" onClick={onLogout} title="Logout">✗</button>
+
+		</div>
+	</div>
+);
+
+export default NavBar;
