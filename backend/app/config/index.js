ce0ee9c3a3eb013495b55bfb4f4c52f9eeafb862
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 02:47:12 +0200

    change backend port

--- backend/app/config/index.js
+++ backend/app/config/index.js
@@ -2,7 +2,7 @@ export default {
     env: 'dev',
     dev: {
         secret: '',
-        port: 3000,
+        port: 3300,
         database: {
             uri: 'mongodb://localhost:27017/test'
         }
