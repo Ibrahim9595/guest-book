535af8fa0bc0fd92a0f3070ee3ed419c61523fda
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 21:36:48 +0200

    add validateAsync to async validation like server checking if email exists

--- frontend/src/components/CustomForm/CustomForm.jsx
+++ frontend/src/components/CustomForm/CustomForm.jsx
@@ -12,26 +12,42 @@ export class CustomForm extends React.PureComponent {
         }
     }
 
-    validate = (values) => {
+    isValid = (errors) => Object.values(errors).every(el => el.length === 0);
+
+    validate = async (values) => {
         const errors = typeof (this.props.validate) === 'function' ? (this.props.validate(values) || {}) : {}
 
-        const isValid = Object.values(errors).every(el => el.length === 0)
+        this.setState({ errors, isValid: this.isValid(errors) });
+    }
 
-        this.setState({ errors, isValid })
+    validateAsync = async (key, value) => {
+        // Check if the target is valid using the sync rules only if the local validataion success will validate through
+        const validSync = !this.state.errors[key];
+
+        if (typeof (this.props.validateAsync) === 'function' && validSync) {
+            this.setState({ isValid: false });
+            const err = await this.props.validateAsync(key, value);
+            const newErrors = { ...this.state.errors, ...err };
+
+            this.setState({ errors: newErrors, isValid: this.isValid(newErrors) });
+        }
     }
 
     handleChange = (event) => {
         this.setState({
             values: { ...this.state.values, [event.target.name]: event.target.value },
         }, () => {
-            this.validate(this.state.values)
-        })
+            this.validate(this.state.values);
+        });
     }
 
     handleBlur = (event) => {
+        const key = event.target.name;
+        const value = event.target.value;
+
         this.setState({
-            dirty: { ...this.state.dirty, [event.target.name]: true }
-        })
+            dirty: { ...this.state.dirty, [key]: true }
+        }, () => this.validateAsync(key, value))
     }
 
     setFormSubmitting = (val) => {
@@ -59,6 +75,7 @@ export class CustomForm extends React.PureComponent {
                 {typeof (this.props.children) === 'function' ? this.props.children({
                     handleChange: this.handleChange,
                     handleBlur: this.handleBlur,
+                    reset: this.reset,
                     values: this.state.values,
                     submitting: this.state.submitting,
                     errors: this.state.errors,
