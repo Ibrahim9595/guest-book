9116a22dfe539cc23fce5ddc3a5580faf57d7db2
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 04:40:03 +0200

    solve auth bug (getting wrong user when passing no token

--- backend/app/middlewares/auth.js
+++ backend/app/middlewares/auth.js
@@ -2,19 +2,26 @@ import { UserModel } from '../models/userModel';
 
 
 export const AUTH = async (req, res, next) => {
+    const wrong = () => {
+        res.status(401).json({
+            status: 'UNAUTHORIZED',
+            error: 'NOT_VALID_USER'
+        });
+    }
+
     try {
         const { authorization } = req.headers;
         const token = authorization.split('Bearer ')[1];
+
+        if (!token) return wrong();
+
         const user = await UserModel.findOneQuery({ token })
 
         if (user) {
             req.user = user;
             next();
         } else {
-            res.status(401).json({
-                status: 'UNAUTHORIZED',
-                error: 'NOT_VALID_USER'
-            });
+            wrong();
         }
     } catch (error) {
         next(error);
