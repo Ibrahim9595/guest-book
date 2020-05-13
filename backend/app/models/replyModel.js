a7a05eb11bd7dd7a273c04ff61297a5767c51d99
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 04:40:48 +0200

    return user_id with replies

--- backend/app/models/replyModel.js
+++ backend/app/models/replyModel.js
@@ -33,6 +33,7 @@ class Reply extends BaseModel {
                     text: 1,
                     created_at: 1,
                     updated_at: 1,
+                    user_id: 1,
                     'user._id': 1,
                     'user.email': 1,
                     'user.name': 1
