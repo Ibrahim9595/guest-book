f43b328cf1aff1e9cb3be708c42eeadd578f1d58
Ibrahim Mamdouh <i.mamdouh@theelephant.tech>
2020-05-13 19:14:47 +0200

    connect login with the userContext

--- frontend/src/screens/LoginScreen/index.jsx
+++ frontend/src/screens/LoginScreen/index.jsx
@@ -3,55 +3,56 @@ import './index.css';
 import { validate } from '../../utils/validate';
 import { CustomForm } from '../../components/CustomForm/CustomForm';
 import { CustomInputField } from '../../components/CustomInputField/CustomInputField';
+import { UserContext } from '../../logic/context/user-context';
 
-const Login = () => (
-    <CustomForm
-        initialValues={{
-            email: '',
-            password: '',
-        }}
-        onSubmit={({ values, setFormSubmitting }) => {
-            setFormSubmitting(true);
-            setTimeout(() => {
-                console.log(values);
-                setFormSubmitting(false);
-            }, 2000);
-        }}
-        validate={(values) =>
-            validate(values, {
-                email: ['required', 'email'],
-                password: ['required', 'minLength:8'],
-            })
-        }
-    >
-        {({
-            handleChange,
-            handleBlur,
-            values,
-            submitting,
-            errors,
-            dirty,
-            isValid,
-        }) => (
-                <>
-                    <CustomInputField label='Email' erros={
-                        dirty.email ? errors.email : []
-                    }
-                        value={values.email}
-                        placeholder='Write your Email'
-                        name='email' onChange={handleChange} onBlur={handleBlur}
-                    />
-                    <CustomInputField label='Password' type='password' erros={
-                        dirty.password ? errors.password : []
-                    }
-                        value={values.password}
-                        placeholder='Password at least 8 characters'
-                        name='password' onChange={handleChange} onBlur={handleBlur}
-                    />
-                    <button className='submit-button' type='submit' disabled={!isValid || submitting}>Submit</button>
-                </>
-            )}
-    </CustomForm>
-);
+const Login = () => {
+    const userContext = React.useContext(UserContext);
+
+    return (
+        <CustomForm
+            initialValues={{
+                email: '',
+                password: '',
+            }}
+            onSubmit={({ values }) => {
+                userContext.login(values.email, values.password);
+            }}
+            validate={(values) =>
+                validate(values, {
+                    email: ['required', 'email'],
+                    password: ['required', 'minLength:8'],
+                })
+            }
+        >
+            {({
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                dirty,
+                isValid,
+            }) => (
+                    <>
+                        {userContext.error && <span className="error-span">Wrong user name or password</span>}
+                        <CustomInputField label='Email' erros={
+                            dirty.email ? errors.email : []
+                        }
+                            value={values.email}
+                            placeholder='Write your Email'
+                            name='email' onChange={handleChange} onBlur={handleBlur}
+                        />
+                        <CustomInputField label='Password' type='password' erros={
+                            dirty.password ? errors.password : []
+                        }
+                            value={values.password}
+                            placeholder='Password at least 8 characters'
+                            name='password' onChange={handleChange} onBlur={handleBlur}
+                        />
+                        <button className='submit-button' type='submit' disabled={!isValid || userContext.loading}>Submit</button>
+                    </>
+                )}
+        </CustomForm>
+    )
+}
 
 export default Login;
